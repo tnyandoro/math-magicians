12fe367ef8b8cbb435580020ba3ddd78b650857d
Tendai <tendai1@hotmail.com>
2021-08-25 11:44:03 +0200

    Add navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import './Navabar.css';
+import './Navbar.css';
 
 function Navbar() {
   const [click, setClick] = useState(false);
 
-  const handleClick = () => setClick(!click);
+  // const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
 
   return (
@@ -13,13 +13,10 @@ function Navbar() {
       <nav className="navbar">
         <div className="navbar-container">
           <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-            TRVL
+            Math magicians
             {' '}
             <i className=" fab fa-typo3" />
           </Link>
-          <div className="menu-icon" onClick={handleClick}>
-            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
-          </div>
           <ul className={click ? 'nav-menu active' : 'nav-menu'}>
             <li className="nav-item">
               <Link to="/" className="nav-links" onClick={closeMobileMenu}>
@@ -36,11 +33,7 @@ function Navbar() {
               </Link>
             </li>
             <li className="nav-item">
-              <Link
-                to="/qoute"
-                className="nav-links"
-                onClick={closeMobileMenu}
-              >
+              <Link to="/qoute" className="nav-links" onClick={closeMobileMenu}>
                 Qoute
               </Link>
             </li>
