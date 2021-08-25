5fc4ec8ae6dbd59e5a76c4e94c01a47f5832fe11
Tendai Nyandoro <tendai1@hotmail.com>
2021-08-25 15:32:53 +0200

    Fix the linters

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,10 +5,7 @@ import './Navbar.css';
 function Navbar() {
   const [click, setClick] = useState(false);
 
-  // const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
-
-  // window.addEventListener('resize');
 
   return (
     <div>
@@ -19,9 +16,6 @@ function Navbar() {
             {' '}
             <i className=" fab fa-typo3" />
           </Link>
-          {/* <div className="menu-icon" onClick={handleClick}>
-            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
-          </div> */}
           <ul className={click ? 'nav-menu active' : 'nav-menu'}>
             <li className="nav-item">
               <Link to="/" className="nav-links" onClick={closeMobileMenu}>
