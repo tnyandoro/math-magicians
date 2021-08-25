ea367b9de6b82f47a3f8d1a4b4a98477b4f90376
Tendai <tendai1@hotmail.com>
2021-08-25 13:28:19 +0200

    Add the style colors for nav

--- src/components/pages/Quote.js
+++ src/components/pages/Quote.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import '../../App.css';
+import './HeroSection.css';
 
 function Qoute() {
   return (
-    <div>
-      <h1>Qoute</h1>
-      <p>
-        As far as the laws of mathematics refer to reality, they are not
-        certain, and as far as they are certain, they do not refer to reality. —
-        Albert Einstein
-      </p>
+    <div className="hero-container">
+      <div className="info">
+        <h1> Qoute</h1>
+        <p>
+          As far as the laws of mathematics refer to reality, they are not
+          certain, and as far as they are certain, they do not refer to reality. —
+          Albert Einstein
+        </p>
+      </div>
     </div>
   );
 }
