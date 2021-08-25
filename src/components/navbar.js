919e64a951d4429f0905f85d71fc8c35123b4bc6
Tendai <tendai1@hotmail.com>
2021-08-25 16:23:53 +0200

    Fix the stupid error

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,6 +1,9 @@
+/* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
+// eslint-disable-next-line import/no-cycle
+import App from '../App';
 
 function Navbar() {
   const [click, setClick] = useState(false);
